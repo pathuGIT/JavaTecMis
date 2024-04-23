c8b83728e6e75d4a90e46d632f457b872ca7a1d1
SaniduSanadakan <126314705+SaniduSanadakan@users.noreply.github.com>
2024-04-23 22:34:54 +0530

    technical officer update

--- src/main/java/org/example/javatecmis/technicalOfficer/techCtrl.java
+++ src/main/java/org/example/javatecmis/technicalOfficer/techCtrl.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.example.javatecmis.LoginController;
 
@@ -19,6 +20,17 @@ public class techCtrl {
 
     @FXML
     private Label l;
+    @FXML
+    private Pane TimetablePane;
+    @FXML
+    private Pane attendplane;
+    @FXML
+    private Pane homePane;
+    @FXML
+    private Pane noticepane;
+    @FXML
+    private Pane profilePane;
+
     @FXML
     void btn(ActionEvent event) {
         l.setText("Kamal");
@@ -36,6 +48,73 @@ public class techCtrl {
     public void logout(ActionEvent event) throws IOException {
         LoginController o = new LoginController();
         o.logout(event);
+    }
+
+
+    @FXML
+    void noticeFun(ActionEvent event) {
+        choosePanel("notice");
+    }
+
+    @FXML
+    void profileFun(ActionEvent event) {
+        choosePanel("profile");
+    }
+
+    @FXML
+    void timeFun(ActionEvent event) {
+        choosePanel("timetable");
+    }
+    @FXML
+    void AttendanceFun(ActionEvent event) {
+        choosePanel("attendance");
+    }
+
+    @FXML
+    void homeFun(ActionEvent event) {
+        choosePanel("home");
+    }
+
+
+    void choosePanel(String  x){
+        switch (x){
+            case "home":
+                TimetablePane.setVisible(false);
+                attendplane.setVisible(false);
+                homePane.setVisible(true);
+                noticepane.setVisible(false);
+                profilePane.setVisible(false);
+
+                break;
+            case "attendance":
+                TimetablePane.setVisible(false);
+                attendplane.setVisible(true);
+                homePane.setVisible(false);
+                noticepane.setVisible(false);
+                profilePane.setVisible(false);
+                break;
+            case "timetable":
+                TimetablePane.setVisible(true);
+                attendplane.setVisible(false);
+                homePane.setVisible(false);
+                noticepane.setVisible(false);
+                profilePane.setVisible(false);
+                break;
+            case "profile":
+                TimetablePane.setVisible(false);
+                attendplane.setVisible(false);
+                homePane.setVisible(false);
+                noticepane.setVisible(false);
+                profilePane.setVisible(true);
+                break;
+            case "notice":
+                TimetablePane.setVisible(false);
+                attendplane.setVisible(false);
+                homePane.setVisible(false);
+                noticepane.setVisible(true);
+                profilePane.setVisible(false);
+                break;
+        }
     }
 
 
